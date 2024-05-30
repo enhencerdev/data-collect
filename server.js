5e39f9cade5fbb43f1d281c4e838e93426eb54ed
orkhanmeh <orkhanmeh@gmail.com>
2024-05-30 10:00:10 +0300

    deployment push

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ require("./app/routes/visitor.routes")(app);
 
 
 // set port, listen for requests
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
