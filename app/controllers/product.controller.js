151549a50ab3cf236b0d02226f12f44455641887
orkhanmeh <orkhanmeh@gmail.com>
2024-06-05 17:37:52 +0300

    still loggin it

--- app/controllers/product.controller.js
+++ app/controllers/product.controller.js
@@ -64,9 +64,9 @@ async function updateVisitorsTable(userId, visitorData) {
       { $match: { _id: new Mongoose.Types.ObjectId(userId) } },
       { $project: { productCategories: 1, "crmDetails.audienceNetworkSwitch": 1 } }
     ]);
-    console.log("----will get user")
+    console.log("---" + userId + "----will get user")
     const user = await userAggregation.exec();
-    console.log("----user geldi ", user)
+    console.log("---" + userId + "----user geldi ", user)
     if (user.length === 0) {
       return { "message": "no user" }
     } else if (!user[0].crmDetails || !user[0].crmDetails.audienceNetworkSwitch) {
@@ -86,7 +86,7 @@ async function updateVisitorsTable(userId, visitorData) {
       
     }
     
-    console.log("----continue")
+    console.log("---" + userId + "----continue")
   } catch (error) {
     throw error;
   }
@@ -96,9 +96,9 @@ async function updateVisitorsTable(userId, visitorData) {
   
   // Save Visitor in the database
   try {
-    console.log("----az kaldiiii")
+    console.log("---" + userId + "----az kaldiiii")
     await Visitor.upsert(visitorData);
-    console.log("----bitti burasi")
+    console.log("---" + userId + "----bitti burasi")
   } catch (error) {
     throw error;
   }
@@ -140,19 +140,19 @@ exports.create = async (req, res) => {
     type,
   };
 
-  console.log("----1")
+  console.log("---" + userID + "----1")
   updateProductData(product, product.type);
   
-  console.log("----2")
+  console.log("---" + userID + "----2")
   
   // Save Product in the database
   if (type === "ecommerce") {
-    console.log("----3")
+    console.log("---" + userID + "----3")
     Product.tableName = "VISITOR_DATA_PRODUCT_" + product.userID;
-    console.log("----4")
+    console.log("---" + userID + "----4")
     try {
       const createdProduct = await Product.upsert(product);
-      console.log("----5 ", createdProduct)
+      console.log("---" + userID + "----5 ", createdProduct)
       
       if (!createdProduct) {
         // If column already exists, update pageCount
@@ -165,7 +165,7 @@ exports.create = async (req, res) => {
           price,
         } = product;
 
-        console.log("----6")
+        console.log("---" + userID + "----6")
         
         try {
           await Product.update(
@@ -181,9 +181,9 @@ exports.create = async (req, res) => {
               },
             }
           );
-          console.log("----88")
+          console.log("---" + userID + "----88")
         } catch (error) {
-          console.log("----nooo")
+          console.log("---" + userID + "----nooo")
           res.status(500).send({
             message: "Error occurred while updating the product.",
             error: error.message,
@@ -191,9 +191,9 @@ exports.create = async (req, res) => {
           return;
         }
       }
-      console.log("----99")
+      console.log("---" + userID + "----99")
       await updateVisitorsTable(userID, { category: product.productCategory2, visitorID: product.visitorID });
-      console.log("----last")
+      console.log("---" + userID + "----last")
       res.status(200).send({ result: "success" });
     } catch (error) {
       res.status(500).send({
