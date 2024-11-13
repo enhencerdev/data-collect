0cfc5ad84eef141910179ea0650807d88dc580af
orkhanmeh <orkhanmeh@gmail.com>
2024-11-13 20:29:29 +0300

    redis password added

--- app/config/redis.js
+++ app/config/redis.js
@@ -3,7 +3,7 @@ const Redis = require('ioredis');
 const redis = new Redis({
   host: process.env.REDIS_HOST || 'localhost',
   port: process.env.REDIS_PORT || 6379,
-//   password: process.env.REDIS_PASSWORD,
+  password: process.env.REDIS_PASSWORD,
   // Optional configurations
   retryStrategy: (times) => {
     const delay = Math.min(times * 50, 2000);
