1493618134c0d003e2ab4900f81b25a3b582b5e5
orkhanmeh <orkhanmeh@gmail.com>
2025-03-21 18:32:45 +0300

    no parsing for body

--- app/util/request-logger.js
+++ app/util/request-logger.js
@@ -12,20 +12,14 @@ const requestLogger = (req, res, next) => {
     console.log("")
     console.log("")
     console.log("================================================")
-    if (req.body) {
-        const body = JSON.parse(req.body)
-        console.log({
-            timestamp,
-            ip: clientIp,
-            method: req.method,
-            path: req.path,
-            userID: body?.userID || body?.userId,
-            userAgent: req.headers['user-agent']
-        });
-    } else {
-        console.log("No body")
-
-    }
+    console.log({
+        timestamp,
+        ip: clientIp,
+        method: req.method,
+        path: req.path,
+        body: req.body,
+        userAgent: req.headers['user-agent']
+    });
 
     res.on('finish', () => {
         const duration = Date.now() - start;
